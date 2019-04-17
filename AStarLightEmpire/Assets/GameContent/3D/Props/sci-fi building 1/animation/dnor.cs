af3fddcadc9713be9e8dfc6c9c80d9e5925266db
ngomes007 <32779546+ngomes007@users.noreply.github.com>
2019-04-17 12:23:16 -0700

    building

---
+++ AStarLightEmpire/Assets/GameContent/3D/Props/sci-fi building 1/animation/dnor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dnor : MonoBehaviour
+{
+    [SerializeField] private Animator dooranim;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            dooranim.SetBool("dooropen", true);
+        }
+    }
+}
