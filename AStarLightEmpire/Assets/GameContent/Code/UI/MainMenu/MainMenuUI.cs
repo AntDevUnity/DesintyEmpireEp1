67e6c957888e82e2c88830c7df6a0d100b0cacfc
Antony Robert Wells <antonyrobwells@gmail.com>
2019-05-13 22:59:48 +0100

    progress being made - main menu working

---
+++ AStarLightEmpire/Assets/GameContent/Code/UI/MainMenu/MainMenuUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuUI : MonoBehaviour
+{
+
+
+    public void BeginGame()
+    {
+        Debug.Log("Game begun!");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
