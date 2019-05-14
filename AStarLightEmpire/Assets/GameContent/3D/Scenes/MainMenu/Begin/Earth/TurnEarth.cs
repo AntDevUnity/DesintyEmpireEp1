a9227b3b769a0094c3376e0b3429ed15e536ffd6
Antony Robert Wells <antonyrobwells@gmail.com>
2019-05-14 01:40:25 +0100

    more work on the main menu

---
+++ AStarLightEmpire/Assets/GameContent/3D/Scenes/MainMenu/Begin/Earth/TurnEarth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnEarth : MonoBehaviour
+{
+
+    public Vector3 Turn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(Turn, Space.Self);    
+    }
+}
