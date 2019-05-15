977120a483485f353ceba21806eb520a5f6c28c1
Antony Wells <antonyrobwells@gmail.com>
2019-05-15 02:31:34 +0100

    added loading screen with progress bar. and other changes

---
+++ AStarLightEmpire/Assets/GameContent/Code/Loading/LoadSceneScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSceneScript : MonoBehaviour
+{
+    public static int LoadIndex;
+
+    [SerializeField]
+    private UnityEngine.UI.Image _prog;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadAsync());
+    }
+
+    IEnumerator LoadAsync()
+    {
+        AsyncOperation loadScene = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(LoadIndex);
+        
+        while(loadScene.progress<1)
+        {
+            _prog.fillAmount = loadScene.progress;
+            yield return new WaitForEndOfFrame();
+        }
+
+        yield return new WaitForEndOfFrame();
+
+    }
+}
